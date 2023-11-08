56e6c278158327539e15559d074566e31574c18d
Gabriel Borsero <gabriel.borsero@gmail.com>
2023-11-08 12:31:15 -0300

    adjust fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,15 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      sans: ["Sometype Mono", "monospace"],
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "synthwave", "cyberpunk"],
-    darkTheme: "cyberpunk", // name of one of the included themes for dark mode
+    themes: ["light", "dracula", "cupcake", "synthwave", "cyberpunk"],
+    darkTheme: "synthwave", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
