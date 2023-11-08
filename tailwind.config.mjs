a2777d70847376b7b0e6761f69acda6d0add928a
Gabriel Borsero <gabriel.borsero@gmail.com>
2023-11-08 11:48:25 -0300

    pixel icons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,8 +6,8 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "synthwave"],
-    darkTheme: "synthwave", // name of one of the included themes for dark mode
+    themes: ["light", "dark", "cupcake", "synthwave", "cyberpunk"],
+    darkTheme: "cyberpunk", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
