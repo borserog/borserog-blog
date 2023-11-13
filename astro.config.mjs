fa1c6c7e721e4d307da90e0d75d6f841f7612ff3
Gabriel Borsero <gabriel.borsero@gmail.com>
2023-11-13 11:09:49 -0300

    initial setup for content querying

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
-  output: "server",
-  adapter: vercel()
+  integrations: [tailwind(), preact(), mdx()],
+  output: "hybrid",
+  adapter: vercel(),
 });
